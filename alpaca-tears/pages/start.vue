b6f8fe2845db5fb84fa38c5034487f63680cc1d3
yokonoji <yokonoji.work@gmail.com>
2020-02-03 00:03:25 +0900

    sort

---
+++ alpaca-tears/pages/start.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    aaa
+  </div>
+</template>
+
+<script>
+export default {
+  // components: {
+  // }
+  data() {
+    return {
+      player: 2,
+      turn: 15
+    }
+  },
+  created: {},
+  methods: {
+    random_sort(array) {
+      for(let i = array.length - 1; i > 0; i--) {
+        let r = Math.floor(Math.random() * (i + 1))
+        let tmp = array[i]
+        array[i] = array[r]
+        array[r] = tmp;
+      }
+    }
+  }
+}
+</script>
+
+<style scoped></style>
